48cfb320e615eae8872a2bd0ed447add239773ee
rfsaldanha <rfsaldanha@gmail.com>
2025-08-06 11:19:16 -0300

    Basic package

--- R/iqar.R
+++ R/iqar.R
@@ -1,12 +1,61 @@
-iqar <- function(mp10 = NA, mp2.5 = NA, o3 = NA, co = NA, no2 = NA, so2 = NA) {
-  max(
-    iqar_gas(mp10, "mp10"),
-    iqar_gas(mp2.5, "mp2.5"),
-    iqar_gas(o3, "o3"),
-    iqar_gas(co, "co"),
-    iqar_gas(no2, "so2"),
-    iqar_gas(so2, "so2"),
+#' Compute IQAr index considering several air pollutants
+#'
+#' This function computes the IQAr index considering several air pollutants using the Brazilian CONAMA/MMA specification.
+#'
+#' @details
+#' Units: mp10: mug/m3, mp2.5: mug/m3, o3: mug/m3, co: PPM, no2: mug/m3, so2: mug/m3
+#'
+#' @param pm10 numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param pm2.5 numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param o3 numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param co numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param no2 numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param so2 numeric. Pollutant concentration. See details bellow for the correct unit.
+#' @param label logical. If `FALSE`, the function will return the IQAr value, if `TRUE` the function will return the IQAr label. Defaults to `FALSE`.
+#'
+#' @returns numeric. IQAr index value or classification.
+#'
+#' @export
+#' @examples
+#' iqar(pm10 = 50, pm2.5 = 80, o3 = 60)
+#' iqar(pm10 = 50, pm2.5 = 80, o3 = 60, label = TRUE)
+iqar <- function(
+  pm10 = NA,
+  pm2.5 = NA,
+  o3 = NA,
+  co = NA,
+  no2 = NA,
+  so2 = NA,
+  label = FALSE
+) {
+  checkmate::assert_numeric(x = pm10)
+  checkmate::assert_numeric(x = pm2.5)
+  checkmate::assert_numeric(x = o3)
+  checkmate::assert_numeric(x = co)
+  checkmate::assert_numeric(x = no2)
+  checkmate::assert_numeric(x = so2)
+
+  res <- max(
+    iqar_pol(x = pm10, pol = "pm10"),
+    iqar_pol(x = pm2.5, pol = "pm2.5"),
+    iqar_pol(x = o3, pol = "o3"),
+    iqar_pol(x = co, pol = "co"),
+    iqar_pol(x = no2, pol = "so2"),
+    iqar_pol(x = so2, pol = "so2"),
     na.rm = TRUE
   ) |>
     round(digits = 0)
+
+  if (label == TRUE) {
+    res <- dplyr::case_when(
+      res >= 0 & res <= 40 ~ "N1 - Boa",
+      res >= 41 & res <= 80 ~ "N2 - Moderada",
+      res >= 81 & res <= 120 ~ "N3 - Ruim",
+      res >= 121 & res <= 200 ~ "N4 - Muito Ruim",
+      res >= 201 & res <= 400 ~ "N5 - Péssima",
+      .default = "Sem classificação."
+    )
+  }
+
+  return(res)
 }
--- R/iqar_pol.R
+++ R/iqar_pol.R
@@ -1,5 +1,28 @@
-iqar_gas <- function(x, gas = "pm2.5") {
-  if (gas == "pm10") {
+#' Compute specific IQAr index to several air pollutants
+#'
+#' This function computes specific IQAr index to several air pollutants using the Brazilian CONAMA/MMA specification.
+#'
+#' @details
+#' Units: pm10: mug/m3, pm2.5: mug/m3, o3: mug/m3, co: PPM, no2: mug/m3, so2: mug/m3
+#'
+#'
+#' @param x numeric. Pollutants concentration. See details bellow for the correct unit.
+#' @param pol character. Pollutant abbreviation: pm10, pm2.5, o3, co, no2, so2.
+#'
+#' @returns numeric. Pollutant specific IQAr index
+#'
+#' @export
+#' @examples
+#' iqar_pol(x = 10, pol = "pm2.5")
+iqar_pol <- function(x, pol = "pm2.5") {
+  checkmate::assert_numeric(x = x)
+  checkmate::assert_choice(
+    x = pol,
+    choices = c("pm10", "pm2.5", "o3", "co", "no2", "so2")
+  )
+
+  lim <- NULL
+  if (pol == "pm10") {
     lim <- dplyr::case_when(
       x >= 0 & x < 45 ~ c(0, 40, 0, 45),
       x >= 45 & x < 100 ~ c(41, 80, 46, 100),
@@ -8,7 +31,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
       x >= 250 ~ c(201, 400, 251, 600),
       .default = NA
     )
-  } else if (gas == "pm2.5") {
+  } else if (pol == "pm2.5") {
     lim <- dplyr::case_when(
       x >= 0 & x < 15 ~ c(0, 40, 0, 15),
       x >= 15 & x < 50 ~ c(41, 80, 16, 50),
@@ -17,7 +40,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
       x >= 125 ~ c(201, 400, 126, 300),
       .default = NA
     )
-  } else if (gas == "o3") {
+  } else if (pol == "o3") {
     lim <- dplyr::case_when(
       x >= 0 & x < 100 ~ c(0, 40, 0, 100),
       x >= 100 & x < 130 ~ c(41, 80, 101, 130),
@@ -26,7 +49,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
       x >= 200 ~ c(201, 400, 201, 800),
       .default = NA
     )
-  } else if (gas == "co") {
+  } else if (pol == "co") {
     lim <- dplyr::case_when(
       x >= 0 & x < 9 ~ c(0, 40, 0, 9),
       x >= 9 & x < 11 ~ c(41, 80, 10, 11),
@@ -35,7 +58,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
       x >= 15 ~ c(201, 400, 16, 50),
       .default = NA
     )
-  } else if (gas == "no2") {
+  } else if (pol == "no2") {
     lim <- dplyr::case_when(
       x >= 0 & x < 200 ~ c(0, 40, 0, 200),
       x >= 200 & x < 240 ~ c(41, 80, 201, 240),
@@ -44,7 +67,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
       x >= 1130 ~ c(201, 400, 1131, 3750),
       .default = NA
     )
-  } else if (gas == "so2") {
+  } else if (pol == "so2") {
     lim <- dplyr::case_when(
       x >= 0 & x < 40 ~ c(0, 40, 0, 40),
       x >= 40 & x < 50 ~ c(41, 80, 41, 50),
@@ -56,14 +79,7 @@ iqar_gas <- function(x, gas = "pm2.5") {
   }
 
   res <- lim[1] + ((lim[2] - lim[1]) / (lim[4] - lim[3])) * (x - lim[3])
+  res <- round(x = res, digits = 0)
 
   return(res)
-}
-
-teste <- function(x) {
-  if (x > 10) {
-    x + 10
-  } else {
-    (x - 10)
-  }
 }
